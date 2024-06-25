aa128e8a2cc70d7edb784a7405940af468e04ff4
9ft6 <9ft6.dev@gmail.com>
2024-06-25 14:06:33 +0700

    refactor bot

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -4,16 +4,5 @@ from bot.bot import FarmBot
 from bot.ws import BotWebSocket
 
 
-class BotApp:
-    bot: FarmBot
-    ws: BotWebSocket
-
-    def __init__(self):
-        self.bot = FarmBot()
-
-    async def run(self):
-        return await asyncio.gather(self.bot.serve())
-
-
 def run_bot():
-    asyncio.run(BotApp().run())
+    asyncio.run(FarmBot().serve())
--- src/bot/bot.py
+++ src/bot/bot.py
@@ -3,45 +3,33 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 from aiogram.client.default import DefaultBotProperties
 
-from core.logger import SubLogger
 from config import cfg
-from bot.ws import BotWebSocket
 from bot import utils, common
 from bot.users import register_users_handlers
 from bot.runners import register_runners_handlers
 
 
-dispatcher: Dispatcher = Dispatcher()
-logger = SubLogger("TGBot")
-logger_rn = SubLogger("runner")
-static = f"https://github.com/9ft6/coins_farm/blob/main/src/server"
-
-
-class FarmBot:
-    bot: Bot
-    ws: BotWebSocket = BotWebSocket()
-
-    def __init__(self):
-        self.bot = Bot(
-            cfg.bot_token,
-            default=DefaultBotProperties(parse_mode='HTML'),
-        )
-        register_users_handlers(dispatcher)
-        register_runners_handlers(dispatcher)
-
-    async def serve(self):
-        await self.ws.connect()
-        await dispatcher.start_polling(self.bot)
-
-    @staticmethod
+def register_base_handlers(dispatcher):
     @dispatcher.message(CommandStart())
     @utils.authorize
     async def send_welcome(message: types.Message):
         await common.show_main_menu(message.from_user.id, message)
 
-    @staticmethod
     @dispatcher.callback_query(F.data == "/start")
     @utils.authorize
     async def send_start(query: types.CallbackQuery):
         await query.answer()
         await common.show_main_menu(query.from_user.id, query.message)
+
+
+class FarmBot(Bot):
+    def __init__(self):
+        props = DefaultBotProperties(parse_mode='HTML')
+        super().__init__(cfg.bot_token, default=props)
+        self.dispatcher: Dispatcher = Dispatcher()
+        register_base_handlers(self.dispatcher)
+        register_runners_handlers(self.dispatcher)
+        register_users_handlers(self.dispatcher)
+
+    async def serve(self):
+        await self.dispatcher.start_polling(self)
